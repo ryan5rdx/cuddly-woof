0f24ffc94d8aaba768bdaf02e22b711a898d3a2f
Ubuntu <ubuntu@ip-172-31-40-246.us-west-2.compute.internal>
2016-04-12 17:21:45 +0000

    fixes

--- rdx5/rdx5/settings.py
+++ rdx5/rdx5/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%ac4d#^#14%k4$ysc_wv$_v-#61saqai1q2+nhia1qg)#am^h0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
